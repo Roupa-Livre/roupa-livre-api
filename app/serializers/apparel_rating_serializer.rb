137e7c8c7f1cd6a2af082d997b3ba5373450245d
Henrique Rangel <hrangel@nucleo235.com.br>
2016-07-08 09:26:48 -0300

    adicionadas info das colunas comentadas ao serializer de rating

--- app/serializers/apparel_rating_serializer.rb
+++ app/serializers/apparel_rating_serializer.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: apparel_ratings
+#
+#  id         :integer          not null, primary key
+#  apparel_id :integer          not null
+#  user_id    :integer          not null
+#  liked      :boolean          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ApparelRatingSerializer < ActiveModel::Serializer
   attributes :id, :apparel_id, :user_id, :liked
 
