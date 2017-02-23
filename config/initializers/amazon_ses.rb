89aea608e7965b6789c751e0f3fc3bf7a877dfa2
Henrique Rangel <hrangel@nucleo235.com.br>
2017-02-23 06:37:58 -0300

    corrigido endereço de envio de emails para west

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :access_key_id     => Rails.application.secrets.aws_access_key_id,
-  :secret_access_key => Rails.application.secrets.aws_secret_access_key
+  :secret_access_key => Rails.application.secrets.aws_secret_access_key,
+  :server => 'email.eu-west-1.amazonaws.com'
