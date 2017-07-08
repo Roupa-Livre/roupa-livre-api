373cd492b1c262dde0504e98d5c39772500b02e6
Henrique Rangel <hrangel@hrangel.com.br>
2017-07-08 07:57:51 -0300

    Adicionado worler para relatorios

---
+++ app/workers/report_worker.rb
@@ -0,0 +1,78 @@
+class ReportWorker
+  include Sidekiq::Worker
+  sidekiq_options :retry => false
+
+  def perform()
+    puts "Sidekiq for ReportWorker RUNNING"
+    UpdateWorker.perform_in(1.day)
+
+    do_perform
+    puts "Sidekiq for UpdateWorker FINISHED"
+  end
+
+  def do_perform
+    Time.use_zone('Brasilia') do
+      currentTime = Time.zone.now
+      AdminMailer.stats(ReportWorker.all_stats).deliver
+      if currentTime.day == 10
+        AdminMailer.user_list(User.all).deliver
+        AdminMailer.apparel_list(Apparel.all).deliver
+      end
+    end
+  end
+
+  def self.all_stats
+    all = {}
+    all[:yesterday] = grouped_stats(Time.new - 1.day)
+    all[:last_two_days] = grouped_stats(Time.new - 2.days)
+    all[:last_week] = grouped_stats(Time.new - 1.week)
+    all[:last_month] = grouped_stats(Time.new - 1.month)
+    all[:before_last_month] = grouped_stats(nil, Time.new - 1.month)
+    all[:overall] = grouped_stats
+    all
+  end
+
+  def self.grouped_stats(start_date = nil, end_date = nil)
+    results = {}
+    results[:date] = start_date if start_date
+    results[:end_date] = end_date if end_date
+    results[:apparels] = stats(Apparel, start_date, end_date)
+    results[:liked_apparel_ratings] = stats(ApparelRating.where(liked: true), start_date, end_date)
+    results[:not_liked_apparel_ratings] = stats(ApparelRating.where.not(liked: true), start_date, end_date)
+    results[:blocked_users] = stats(BlockedUser, start_date, end_date)
+    results[:chats] = stats(Chat, start_date, end_date)
+    results[:chat_messages] = stats(ChatMessage, start_date, end_date)
+    results[:user] = stats(User, start_date, end_date)
+    results
+  end
+
+  def self.stats(resource, start_date, end_date)
+    if start_date && end_date
+      resource.where('created_at > ? && created_at <= ?', start_date, end_date).count
+    elsif start_date
+      resource.where('created_at > ?', start_date).count
+    elsif end_date
+      resource.where('created_at <= ?', end_date).count
+    else
+      resource.all.count
+    end
+  end
+
+  def self.start
+    set = Sidekiq::ScheduledSet.new
+    jobs = set.select {|job| job.klass == 'UpdateWorker' }
+    if jobs.length == 0
+      Time.use_zone('Brasilia') do
+        currentTime = Time.zone.now
+
+        scheduleTime = Time.zone.local(currentTime.year,currentTime.month, currentTime.day, 10, 0, 0)
+        if scheduleTime <= currentTime
+          scheduleTime = scheduleTime + 1.day
+        end
+
+        ReportWorker.perform_at(scheduleTime)
+      end
+    end
+  end
+
+end
--- lib/tasks/report.rake
+++ lib/tasks/report.rake
@@ -1,11 +1,11 @@
 namespace :report do
   desc "Stats mail sender"
   task stats: :environment do
-    AdminMailer.stats(all_stats).deliver
+    AdminMailer.stats(ReportWorker.all_stats).deliver
   end
 
   task stats_console: :environment do
-    puts all_stats.to_json
+    puts ReportWorker.all_stats.to_json
   end
 
   task users: :environment do
@@ -14,43 +14,6 @@ namespace :report do
 
   task apparels: :environment do
     AdminMailer.apparel_list(Apparel.all).deliver
-  end
-
-  def all_stats
-    all = {}
-    all[:yesterday] = grouped_stats(Time.new - 1.day)
-    all[:last_two_days] = grouped_stats(Time.new - 2.days)
-    all[:last_week] = grouped_stats(Time.new - 1.week)
-    all[:last_month] = grouped_stats(Time.new - 1.month)
-    all[:before_last_month] = grouped_stats(nil, Time.new - 1.month)
-    all[:overall] = grouped_stats()
-    all
-  end
-
-  def grouped_stats(start_date = nil, end_date = nil)
-    results = {}
-    results[:date] = start_date if start_date
-    results[:end_date] = end_date if end_date
-    results[:apparels] = stats(Apparel, start_date, end_date)
-    results[:liked_apparel_ratings] = stats(ApparelRating.where(liked: true), start_date, end_date)
-    results[:not_liked_apparel_ratings] = stats(ApparelRating.where.not(liked: true), start_date, end_date)
-    results[:blocked_users] = stats(BlockedUser, start_date, end_date)
-    results[:chats] = stats(Chat, start_date, end_date)
-    results[:chat_messages] = stats(ChatMessage, start_date, end_date)
-    results[:user] = stats(User, start_date, end_date)
-    results
-  end
-
-  def stats(resource, start_date, end_date)
-    if start_date && end_date
-      resource.where('created_at > ? && created_at <= ?', start_date, end_date).count
-    elsif start_date
-      resource.where('created_at > ?', start_date).count
-    elsif end_date
-      resource.where('created_at <= ?', end_date).count
-    else
-      resource.all.count
-    end
   end
 
 end
