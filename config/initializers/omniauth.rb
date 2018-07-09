24e5741f85554faaf9b06314ce7c04077bd83976
Henrique Rangel <hrangel@nucleo235.com.br>
2018-07-09 16:35:49 -0300

    alterada url de inicializacao omniauth facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,      Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret
+  provider :facebook,      Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret, client_options: {
+    site: 'https://graph.facebook.com/v2.10',
+    authorize_url: "https://www.facebook.com/v2.10/dialog/oauth"
+  }
 end
